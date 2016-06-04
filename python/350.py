1cfa1922a8ad3a46a77c567c396d231d2eef1095
lucyking <joehisaishi1943@gmail.com>
2016-06-04 19:38:21 +0800

    349 && 350.py 求交集

---
+++ python/350.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#*********************************************#
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        list4=[]
+        for a in nums1:
+            num1 = nums1.count(a)
+            num2 = nums2.count(a)
+            re = min(num1,num2)
+            # print '>>>',re
+            if re and a not in list4:
+                for i in range(re):  # duplicate times
+                    list4.append(a)
+        return list4
+#*********************************************#
