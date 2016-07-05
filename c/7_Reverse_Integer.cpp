0299687bbfb48ebffd1bc5a1719e4ef0e1250ad0
lucyking <joehisaishi1943@gmail.com>
2016-07-06 01:13:17 +0800

    return (long)re > INT32_MAX?0:int(re);
    Signed-off-by: lucyking <joehisaishi1943@gmail.com>

---
+++ c/7_Reverse_Integer.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int reverse(int x) {
+        long int result = 0;
+        bool is_pos = true;
+        if (x < 0) {
+            is_pos = false;
+            x = -x;
+        }
+        while (x >= 1) {
+            result = result * 10 + x % 10;
+            x = x / 10;
+        }
+
+        if(result > INT32_MAX )
+            return -1;
+        result = (int)result;
+
+        return is_pos ? result : -result;
+    }
+};
+
+int main() {
+    cout << "Hello, World!" << endl;
+
+    Solution Alpha;
+    cout << INT32_MAX <<endl;
+    cout << Alpha.reverse(100) <<endl;
+    cout << Alpha.reverse(101) <<endl;
+    cout << Alpha.reverse(-101) <<endl;
+    cout << Alpha.reverse(1534236469) <<endl;
+    return 0;
+}
