5006d9dca1e93eef03da0ce2408d44e5088ce25d
lucyking <joehisaishi1943@gmail.com>
2016-09-13 15:46:54 +0800

    list+[] | list.append([])

--- python/test/String-List.py
+++ python/test/String-List.py
@@ -1,15 +1,23 @@
+# -*- encoding:utf-8 -*-
 if __name__ == '__main__':
-    l3= [[], ['A'], ['B'], ['C'], ['A', 'A'], ['A', 'B'],['A','A','A']]
-    re=[]
+    myList= [[],['A'], ['A', 'B'],['A','B','C']]
+    re=['#1']
 
-    print ['A'] in l3
-    print type(l3[-1])
-    print ''.join(str(l3[-1]))
+    print ['A'] in myList
+    print type(myList[-1])
+    tmp = ''.join(myList[-1])
+    print tmp
+    print type(tmp)
 
-    for item in l3:
+    for item in myList:
         if len(item)==3:
-            re.append(item)
+            re+=item            # ['#1',+,+,+] 直接在末尾追加
+            re.append(item)     # ['#1',[+,+,+]] 作为新的元素添加
 
-    print re
-    # l33=filter(lambda x:len(x)!=3,l3)
-    # print l33
+    print '>>>',re
+    
+    print len([['123']]) # ->1
+    print len('123') # ->3
+
+    # myList3=filter(lambda x:len(x)!=3,myList)
+    # print myList3
