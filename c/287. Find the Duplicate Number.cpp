0fd9a0ef9ea4932d19166705d4f5d599ebbce5ff
lucyking <joehisaishi1943@gmail.com>
2016-10-05 18:21:53 +0800

    using Set<int> 剔除重复：s.find(key)!= s.end()
    Signed-off-by: lucyking <joehisaishi1943@gmail.com>

--- c/287. Find the Duplicate Number.cpp
+++ c/287. Find the Duplicate Number.cpp
@@ -1,10 +1,29 @@
-class Solution {
+class Solution_dict {
 public:
     int findDuplicate(vector<int>& nums) {
+	//using dict 
         map<int,int> dic;
         for(int i=0;i<nums.size();i++){
             if(dic.find(nums[i])==dic.end()){
                 dic[nums[i]]=1;
+            }
+            else{
+                return nums[i];
+            }
+        }
+        
+    return -1;
+    }
+};
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+	//using Set<int>
+        set<int> s;
+        for(int i=0;i<nums.size();i++){
+            if(s.find(nums[i])==s.end()){
+                s.insert(nums[i]);
             }
             else{
                 return nums[i];
